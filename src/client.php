240ed53a414f38073e8529d350b8bff563dbae50
Ihab Zeedia <ihab.zeedia@gmail.cm>
2021-07-14 14:47:06 -0700

    no auth.

--- src/client.php
+++ src/client.php
@@ -16,7 +16,7 @@ $this->emLog('inside src/client.php');
     Client.downloadLinks = <?php echo !is_null($this->getDownloadLinks()) ? json_encode($this->getDownloadLinks()) : []; ?>;
     Client.filesPath = <?php echo !is_null($this->getFilesPath()) ? json_encode($this->getFilesPath()) : []; ?>;
     Client.getSignedURLAjax = "<?php echo $this->getUrl('ajax/get_signed_url.php', false, true) . '&NOAUTH&pid=' . $this->getProjectId() ?>"
-    Client.saveRecordURLAjax = "<?php echo $this->getUrl('ajax/save_record.php', false, true) . '&pid=' . $this->getProjectId() ?>"
+    Client.saveRecordURLAjax = "<?php echo $this->getUrl('ajax/save_record.php', false, true) . '&NOAUTH&pid=' . $this->getProjectId() ?>"
     Client.recordId = "<?php echo $this->getRecordId() ?: '' ?>"
     Client.eventId = "<?php echo $this->getEventId() ?>"
     Client.instanceId = "<?php echo $this->getInstanceId() ?>"
