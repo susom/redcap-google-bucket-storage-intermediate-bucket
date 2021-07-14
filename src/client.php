d78fd244cc6ffdf9101bbb4751a3f519d5feb24f
Ihab Zeedia <ihab.zeedia@gmail.cm>
2021-07-14 14:18:03 -0700

    no auth.

--- src/client.php
+++ src/client.php
@@ -15,8 +15,8 @@ $this->emLog('inside src/client.php');
     Client.fields = <?php echo json_encode($this->getFields()); ?>;
     Client.downloadLinks = <?php echo !is_null($this->getDownloadLinks()) ? json_encode($this->getDownloadLinks()) : []; ?>;
     Client.filesPath = <?php echo !is_null($this->getFilesPath()) ? json_encode($this->getFilesPath()) : []; ?>;
-    Client.getSignedURLAjax = "<?php echo $this->getUrl('ajax/get_signed_url.php', true, true) . '&pid=' . $this->getProjectId() ?>"
-    Client.saveRecordURLAjax = "<?php echo $this->getUrl('ajax/save_record.php', true, true) . '&pid=' . $this->getProjectId() ?>"
+    Client.getSignedURLAjax = "<?php echo $this->getUrl('ajax/get_signed_url.php', false, true) . '&pid=' . $this->getProjectId() ?>"
+    Client.saveRecordURLAjax = "<?php echo $this->getUrl('ajax/save_record.php', false, true) . '&pid=' . $this->getProjectId() ?>"
     Client.recordId = "<?php echo $this->getRecordId() ?: '' ?>"
     Client.eventId = "<?php echo $this->getEventId() ?>"
     Client.instanceId = "<?php echo $this->getInstanceId() ?>"
