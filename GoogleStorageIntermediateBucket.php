6d16aae0778d2e0e6f0f01c8be52f01537888e78
ihabz <ihab.zeedia@stanford.edu>
2022-05-09 15:12:58 -0700

    fix compatibility issues.

--- GoogleStorageIntermediateBucket.php
+++ GoogleStorageIntermediateBucket.php
@@ -525,8 +525,10 @@ class GoogleStorageIntermediateBucket extends \ExternalModules\AbstractExternalM
     /**
      * @param string $recordId
      */
-    public function setRecordId($recordId)
+    public function setRecordId()
     {
+        $temp = func_get_args();
+        $recordId = $temp[0];
         $this->recordId = $recordId;
     }
 
